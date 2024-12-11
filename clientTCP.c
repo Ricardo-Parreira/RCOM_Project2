15c4a4787ea7c96df96104cd9f69df7e46d85146
martagfmartins <145374939+martagfmartins@users.noreply.github.com>
2024-12-11 19:30:42 +0000

    Update clientTCP.c

--- clientTCP.c
+++ clientTCP.c
@@ -3,33 +3,22 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <errno.h>
 
-int connectToServer_TCP(const char *hostname, int port) {
+// Function to create a TCP connection
+int create_connection(const char *ip, int port) {
     int sockfd;
-    struct sockaddr_in serverAddr;
+    struct sockaddr_in server_addr;
 
-    // Criar socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Error creating socket");
         return -1;
     }
 
-    // Configurar endereço do servidor
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
-    // Verificar se hostname já é um endereço IP
-    if (inet_pton(AF_INET, hostname, &serverAddr.sin_addr) <= 0) {
-        fprintf(stderr, "Error: Invalid IP address or hostname: %s\n", strerror(errno));
-        close(sockfd);
-        return -1;
-    }
-
-    // Conectar ao servidor
-    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error connecting to server");
         close(sockfd);
         return -1;
